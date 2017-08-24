30202df27f9845d157075362c1bd9c9e449ee065
planetarydev <planetarydevelop@gmail.com>
2017-08-24 08:26:01 +0200

    identifier with reverse helpers

--- test/select.js
+++ test/select.js
@@ -1,34 +1,34 @@
-"use strict";
+'use strict';
 
-const expect     = require("chai").expect;
+const expect     = require('chai').expect;
 const SQLBuilder = require('../index');
 const SQLQuery   = require('../lib/sqlquery');
 
 var sqlbuilder   = new SQLBuilder('mysql');
 
-describe("Check Helpers", function() {
-    it("$select", function() {
-        expect('$select' in sqlbuilder._helpers).to.equal(true);
-    });
+describe('Check Helpers', function() {
+	it('$select', function() {
+		expect('$select' in sqlbuilder._helpers).to.equal(true);
+	});
 });
 
 
-describe("SELECT Statements", function() {
-    describe("Simple", function() {
+describe('SELECT Statements', function() {
+	describe('Simple', function() {
 
-        it("Only Table", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $table: 'people'
-                }
-            });
+		it('Only Table', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$table: 'people'
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT * FROM `people`');
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `people`');
 			expect(query.values.length).to.equal(0);
-        });
+		});
 
-		it("Table with alias", function() {
+		it('Table with alias', function() {
 			var query = sqlbuilder.build({
 				$select: {
 					$table: { people: { $as: 'alias_people' } }
@@ -41,154 +41,175 @@ describe("SELECT Statements", function() {
 		});
 
 
-        it("Table with columns as array of strings", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $columns: ['first_name', 'last_name'],
-                    $table: 'people'
-                }
-            });
+		it('Table with columns as objects', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$columns: {
+						fixvalue: 'foo',
+						first_name: { $val: 'John' },
+						last_name: { $as: 'alias_last_name' },
+						gender: { $val: 'male' }
+					},
+					$table: 'people'
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT `first_name`, `last_name` FROM `people`');
-            expect(query.values.length).to.equal(0);
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT ? AS `fixvalue`, ? AS `first_name`, `last_name` AS `alias_last_name`, ? AS `gender` FROM `people`');
+			expect(query.values.length).to.equal(3);
+			expect(query.values[0]).to.equal('foo');
+			expect(query.values[1]).to.equal('John');
+			expect(query.values[2]).to.equal('male');
+		});
 
-        it("Table with columns as object", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $columns: [
-                        { first_name: { $val: 'John' } },
-                        { last_name: { $as: 'alias_last_name' } },
-                        { gender: { $val: 'male' } }
-                    ],
-                    $table: 'people'
-                }
-            });
+		it('Table with columns as array of strings', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$columns: ['first_name', 'last_name'],
+					$table: 'people'
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT ? AS `first_name`, `last_name` AS `alias_last_name`, ? AS `gender` FROM `people`');
-            expect(query.values.length).to.equal(3);
-            expect(query.values[0]).to.equal('John');
-            expect(query.values[1]).to.equal('Doe');
-            expect(query.values[2]).to.equal('male');
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT `first_name`, `last_name` FROM `people`');
+			expect(query.values.length).to.equal(0);
+		});
 
-        it("Table with simple WHERE clause", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $table: 'people',
-                    $where: {
-                        first_name: 'John',
-                        last_name: 'Doe'
-                    }
-                }
-            });
+		it('Table with columns as array of objects', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$columns: [
+						{ first_name: { $val: 'John' } },
+						{ last_name: { $as: 'alias_last_name' } },
+						{ gender: { $val: 'male' } }
+					],
+					$table: 'people'
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? AND `last_name` = ?');
-            expect(query.values.length).to.equal(2);
-            expect(query.values[0]).to.equal('John');
-            expect(query.values[1]).to.equal('Doe');
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT ? AS `first_name`, `last_name` AS `alias_last_name`, ? AS `gender` FROM `people`');
+			expect(query.values.length).to.equal(2);
+			expect(query.values[0]).to.equal('John');
+			expect(query.values[1]).to.equal('male');
+		});
 
-        it("Table with simple WHERE clause, $eq", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $table: 'people',
-                    $where: {
-                        first_name: 'John',
-                        last_name: { $eq: 'Doe' }
-                    }
-                }
-            });
+		it('Table with simple WHERE clause', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$table: 'people',
+					$where: {
+						first_name: 'John',
+						last_name: 'Doe'
+					}
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? AND `last_name` = ?');
-            expect(query.values.length).to.equal(2);
-            expect(query.values[0]).to.equal('John');
-            expect(query.values[1]).to.equal('Doe');
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? AND `last_name` = ?');
+			expect(query.values.length).to.equal(2);
+			expect(query.values[0]).to.equal('John');
+			expect(query.values[1]).to.equal('Doe');
+		});
 
-		it("Table with WHERE clause, $and, $or, $eq", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $table: 'people',
-                    $where: {
+		it('Table with simple WHERE clause, $eq', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$table: 'people',
+					$where: {
+						first_name: 'John',
+						last_name: { $eq: 'Doe' }
+					}
+				}
+			});
+
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? AND `last_name` = ?');
+			expect(query.values.length).to.equal(2);
+			expect(query.values[0]).to.equal('John');
+			expect(query.values[1]).to.equal('Doe');
+		});
+
+		it('Table with WHERE clause, $and, $or, $eq', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$table: 'people',
+					$where: {
 						$and : [
 							{ first_name: 'John' },
-	                        { last_name: { $eq: 'Doe' } },
+							{ last_name: { $eq: 'Doe' } },
 							{ $or : [
 								{ age : { $gt: 18 } },
-	                            { gender : { $ne: 'female' } }
+								{ gender : { $ne: 'female' } }
 							]}
 						]
-                    }
-                }
-            });
+					}
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? AND `last_name` = ? AND (`age` > ? OR `gender` != ?)');
-            expect(query.values.length).to.equal(4);
-            expect(query.values[0]).to.equal('John');
-            expect(query.values[1]).to.equal('Doe');
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? AND `last_name` = ? AND (`age` > ? OR `gender` != ?)');
+			expect(query.values.length).to.equal(4);
+			expect(query.values[0]).to.equal('John');
+			expect(query.values[1]).to.equal('Doe');
 			expect(query.values[2]).to.equal(18);
 			expect(query.values[3]).to.equal('female');
-        });
+		});
 
-		it("Table with simple WHERE clause only $or", function() {
-            var query = sqlbuilder.build({
-                $select: {
-                    $table: 'people',
-                    $where: {
+		it('Table with simple WHERE clause only $or', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$table: 'people',
+					$where: {
 						$or: [
 							{ first_name: 'John' },
-	                        { last_name: { $eq: 'Doe' } }
+							{ last_name: { $eq: 'Doe' } }
 						]
-                    }
-                }
-            });
+					}
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? OR `last_name` = ?');
-            expect(query.values.length).to.equal(2);
-            expect(query.values[0]).to.equal('John');
-            expect(query.values[1]).to.equal('Doe');
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `people` WHERE `first_name` = ? OR `last_name` = ?');
+			expect(query.values.length).to.equal(2);
+			expect(query.values[0]).to.equal('John');
+			expect(query.values[1]).to.equal('Doe');
+		});
 
-		it("Table with GROUP BY", function() {
-            var query = sqlbuilder.build({
-                $select: {
+		it('Table with GROUP BY', function() {
+			var query = sqlbuilder.build({
+				$select: {
 					$columns: ['first_name', 'last_name'],
-                    $table: 'people',
-                    $groupBy: ['first_name', 'last_name']
-                }
-            });
+					$table: 'people',
+					$groupBy: ['first_name', 'last_name']
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT `first_name`, `last_name` FROM `people` GROUP BY `first_name`, `last_name`');
-            expect(query.values.length).to.equal(0);
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT `first_name`, `last_name` FROM `people` GROUP BY `first_name`, `last_name`');
+			expect(query.values.length).to.equal(0);
+		});
 
-		it("Table with GROUP BY and HAVING clause", function() {
-            var query = sqlbuilder.build({
-                $select: {
+		it('Table with GROUP BY and HAVING clause', function() {
+			var query = sqlbuilder.build({
+				$select: {
 					$columns: [
 						'first_name',
 						{ first_name_count: { $count: '*' } }
 					],
-                    $table: 'people',
-                    $groupBy: ['first_name'],
+					$table: 'people',
+					$groupBy: ['first_name'],
 					$having: {
-						first_name_count: { $gt: 2 }
+						$expr: { $count: '*', $gt: 2 }
 					}
-                }
-            });
+				}
+			});
 
-            expect(query).to.be.instanceOf(SQLQuery);
-            expect(query.sql).to.equal('SELECT `first_name`, COUNT(*) AS `first_name_count` FROM `people` GROUP BY `first_name` HAVING COUNT(*) > 2');
-            expect(query.values.length).to.equal(0);
-        });
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT `first_name`, COUNT(*) AS `first_name_count` FROM `people` GROUP BY `first_name` HAVING COUNT(*) > ?');
+			expect(query.values.length).to.equal(1);
+			expect(query.values[0]).to.equal(2);
+		});
 
-    });
+	});
 });
