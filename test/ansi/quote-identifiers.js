dbf6adce2ccd2bb1c6a61d9643ed239d6865c95d
planetarydev <planetarydevelop@gmail.com>
2017-10-14 08:32:40 +0200

    enhance quote identifiers for schema.table

---
+++ test/ansi/quote-identifiers.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const expect     = require('chai').expect;
+const SQLBuilder = require('../../index');
+const SQLQuery   = require('../../lib/sqlquery');
+
+var sqlbuilder   = new SQLBuilder();
+
+describe('Quote identifiers', function() {
+	describe('$table, $from : `table-identifier`', function() {
+		it('should return DELETE FROM `table-identifier`', function() {
+			var query = sqlbuilder.build({
+				$delete: {
+					$table: 'people'
+				}
+			});
+
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('DELETE FROM `people`');
+			expect(query.values.length).to.equal(0);
+		});
+
+		it('should return SELECT * FROM `table-identifier`', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$from: 'people'
+				}
+			});
+
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `people`');
+		});
+	});
+
+	describe('$table, $from : `schema`.`table-identifier`', function() {
+		it('should return DELETE FROM `schema`.`table-identifier`', function() {
+			var query = sqlbuilder.build({
+				$delete: {
+					$table: 'public.people'
+				}
+			});
+
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('DELETE FROM `public`.`people`');
+			expect(query.values.length).to.equal(0);
+		});
+
+		it('should return SELECT * FROM `schema`.`table-identifier`', function() {
+			var query = sqlbuilder.build({
+				$select: {
+					$from: 'public.people'
+				}
+			});
+
+			expect(query).to.be.instanceOf(SQLQuery);
+			expect(query.sql).to.equal('SELECT * FROM `public`.`people`');
+		});
+	});
+
+});
